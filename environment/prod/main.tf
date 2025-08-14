b89cb1d6400e4c6ddb0ab961259d0d82828d2895
boo <dicksonagbesi@gmail.com>
2025-08-14 10:02:13 +0000

    main

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "/Users/mac/Infra-as-Code-IaC-for-a-Web-Application/modules/ec2"
+  source = "./modules/ec2"
   ami = var.ami_id
   instance_type = var.instance_type
 }
--- main.tf
+++ main.tf
@@ -6,3 +6,21 @@ provider "vault"{
   token = ""
 }
 
+module "ec2" {
+  source = "./modules/ec2"
+  ami_id = ""
+  instance_type = "t2.micro"
+}
+
+module "rds" {
+  source = "./modules/rds"
+  allocated_storage    = 
+  storage_type         = 
+  engine               = 
+  engine_version       = 
+  instance_class       = 
+  db_name              = 
+  username             = 
+  password             = 
+  parameter_group_name = 
+}
